4f9b582c243dbe92ac06cdfdf28fc9ec802b746a
jmoulin <jmoulin@castelis.com>
2017-09-11 16:59:46 +0200

    Add HentaiParadise

---
+++ src/Driver/HentaiParadise.php
@@ -0,0 +1,41 @@
+<?php
+namespace SiteDl\Driver;
+
+class HentaiParadise extends \SiteDl\DriverAbstract
+{
+    private $aMatches = [];
+    const DOMAIN = 'hentai-paradise.fr';
+    //https://hentai-paradise.fr/doujins/saimin-seishidou-2-kurashiki-reina-no-baai-fr
+
+    public function canHandle()
+    {
+        return preg_match(
+            '~^https?://' . strtr(self::DOMAIN, ['.' => '\.', '-' => '\-']) . '/doujins/(?<album>[^?/]+)~',
+            $this->sUrl,
+            $this->aMatches
+        );
+    }
+
+    public function getDownloadables()
+    {
+        $oRes = $this->getClient()->request('GET', $this->sUrl . '/0');
+        $aReturn = [];
+        foreach ($this->getDomParser()->load((string)$oRes->getBody())->find('.goPage a') as $oLink) {
+            /**
+             * @var \DOMElement $oLink
+             * @var \DOMElement $oImg
+             */
+            $oImg = $this->getDomParser()
+                ->load((string)$this->getClient()->request('GET', $this->sUrl . '/' . $oLink->getAttribute('href'))->getBody())
+                ->find('#fullPage img');
+            $sFilename = $this->sUrl . '/' .$oImg->getAttribute('src');
+            $aReturn[$this->getFolder(). DIRECTORY_SEPARATOR . basename($sFilename)] = $sFilename;
+        }
+        return $aReturn;
+    }
+
+    private function getFolder()
+    {
+        return implode(DIRECTORY_SEPARATOR, [self::DOMAIN, $this->aMatches['album']]);
+    }
+}
