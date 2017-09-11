3334d280a38051e89c753a5875515d896e522374
jmoulin <jmoulin@castelis.com>
2017-09-11 13:58:05 +0200

    Fixes simply-hentai.com driver

--- src/Driver/SimplyHentai.php
+++ src/Driver/SimplyHentai.php
@@ -9,7 +9,7 @@ class SimplyHentai extends \SiteDl\DriverAbstract
     public function canHandle()
     {
         return preg_match(
-            '~^https?://(?<domain>[^.]\.' . strtr(self::DOMAIN, ['.' => '\.']) . ')/(?<album>[^/]+)/?.*~',
+            '~^https?://(?<domain>[^.]+\.' . strtr(self::DOMAIN, ['.' => '\.', '-' => '\-']) . '/([^?]+))~',
             $this->sUrl,
             $this->aMatches
         );
@@ -17,7 +17,7 @@ class SimplyHentai extends \SiteDl\DriverAbstract
 
     public function getDownloadables()
     {
-        $oRes = $this->getClient()->request('GET', 'http://' . $this->aMatches['domain'] . '/' . $this->aMatches['album'] . '/all-pages');
+        $oRes = $this->getClient()->request('GET', 'http://' . $this->aMatches['domain'] . '/all-pages');
         $aReturn = [];
         foreach ($this->getDomParser()->load((string)$oRes->getBody())->find('a.image-preview') as $oLink) {
             /**
