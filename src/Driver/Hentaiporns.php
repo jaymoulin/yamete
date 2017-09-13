062f65d33a0484f317ebbdfecc7246c83ae179e5
jmoulin <jmoulin@castelis.com>
2017-09-13 19:53:05 +0200

    Added Hentaiporns driver but need to check as 503 are frequent

--- src/Driver/Hentaiporns.php
+++ src/Driver/Hentaiporns.php
@@ -5,12 +5,12 @@ namespace SiteDl\Driver;
 class Hentaiporns extends \SiteDl\DriverAbstract
 {
     private $aMatches = [];
-    const DOMAIN = 'hentaiporns.net';
+    const DOMAIN = 'nhentai.net';
 
     public function canHandle()
     {
         return preg_match(
-            '~^https?://' . strtr(self::DOMAIN, ['.' => '\.']) . '/(?<album>[^/]+)/$~',
+            '~^https?://' . strtr(self::DOMAIN, ['.' => '\.']) . '/g/(?<album>[^/]+)/$~',
             $this->sUrl,
             $this->aMatches
         );
@@ -21,14 +21,20 @@ class Hentaiporns extends \SiteDl\DriverAbstract
         $oRes = $this->getClient()->request('GET', $this->sUrl);
         $aReturn = [];
         $i = 0;
-        foreach ($this->getDomParser()->load((string)$oRes->getBody())->find('.gallery-icon a') as $oImg) {
+        foreach ($this->getDomParser()->load((string)$oRes->getBody())->find('a.gallerythumb') as $oLink) {
             /**
-             * @var \DOMElement $oImg
+             * @var \DOMElement $oLink
              */
-            $sFilename = $oImg->getAttribute('href');
-            $sPath = $this->getFolder() . DIRECTORY_SEPARATOR . str_pad(++$i, 5, '0', STR_PAD_LEFT) . '-'
-                . basename($sFilename);
-            $aReturn[$sPath] = $sFilename;
+            $oRes = $this->getClient()->request('GET', 'https://' . self::DOMAIN . $oLink->getAttribute('href'));
+            foreach ($this->getDomParser()->load((string)$oRes->getBody())->find('#image-container img') as $oImg) {
+                /**
+                 * @var \DOMElement $oImg
+                 */
+                $sFilename = $oImg->getAttribute('src');
+                $sPath = $this->getFolder() . DIRECTORY_SEPARATOR . str_pad(++$i, 5, '0', STR_PAD_LEFT) . '-'
+                    . basename($sFilename);
+                $aReturn[$sPath] = $sFilename;
+            }
         }
         return $aReturn;
     }
