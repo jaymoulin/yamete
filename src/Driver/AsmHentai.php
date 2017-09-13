8a27d1364fc232918b46b0cfb425278eec4326c8
jmoulin <jmoulin@castelis.com>
2017-09-13 17:08:54 +0200

    Add AsmHentai driver

---
+++ src/Driver/AsmHentai.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace SiteDl\Driver;
+
+class AsmHentai extends \SiteDl\DriverAbstract
+{
+    private $aMatches = [];
+    const DOMAIN = 'asmhentai.com';
+
+    public function canHandle()
+    {
+        return preg_match(
+            '~^https?://(' . strtr(self::DOMAIN, ['.' => '\.']) . ')/g/(?<album>[^/?]+)/?~',
+            $this->sUrl,
+            $this->aMatches
+        );
+    }
+
+    public function getDownloadables()
+    {
+        $oRes = $this->getClient()
+            ->request('GET', 'https://' . self::DOMAIN . '/gallery/' . $this->aMatches['album'] . '/1/');
+        $aReturn = [];
+        foreach ($this->getDomParser()->load((string)$oRes->getBody())->find('#jumpto_down option') as $oLink) {
+            /**
+             * @var \DOMElement $oLink
+             */
+            if (empty($oLink->getAttribute('value'))) {
+                continue;
+            }
+            $sLink = 'https://' . self::DOMAIN . $oLink->getAttribute('value');
+            $oRes = $this->getClient()->request('GET', $sLink);
+            foreach ($this->getDomParser()->load((string)$oRes->getBody())->find('#img img') as $oImg) {
+                /**
+                 * @var \DOMElement $oImg
+                 */
+                $sFilename = 'https:' . $oImg->getAttribute('src');
+                $aReturn[$this->getFolder() . DIRECTORY_SEPARATOR . basename($sFilename)] = $sFilename;
+            }
+        }
+        return $aReturn;
+    }
+
+    private function getFolder()
+    {
+        return implode(DIRECTORY_SEPARATOR, [self::DOMAIN, $this->aMatches['album']]);
+    }
+}
