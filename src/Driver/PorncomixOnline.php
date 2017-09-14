9686acb4453fe81c58a6a4a2aaf08669d595f5b6
jmoulin <jmoulin@castelis.com>
2017-09-14 15:19:10 +0200

    Add Porncomixonline driver

---
+++ src/Driver/PorncomixOnline.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace SiteDl\Driver;
+
+
+class PorncomixOnline extends \SiteDl\DriverAbstract
+{
+    private $aMatches = [];
+    const DOMAIN = 'porncomixonline.net';
+
+    public function canHandle()
+    {
+        return preg_match(
+            '~^https?://www\.' . strtr(self::DOMAIN, ['.' => '\.']) . '/(?<album>[^/]+)/$~',
+            $this->sUrl,
+            $this->aMatches
+        );
+    }
+
+    public function getDownloadables()
+    {
+        $oRes = $this->getClient()->request('GET', $this->sUrl);
+        $aReturn = [];
+        foreach ($this->getDomParser()->load((string)$oRes->getBody())->find('.unite-gallery img') as $oImg) {
+            /**
+             * @var \DOMElement $oImg
+             */
+            $sFilename = $oImg->getAttribute('data-image');
+            $aReturn[$this->getFolder() . DIRECTORY_SEPARATOR . basename($sFilename)] = $sFilename;
+        }
+        return $aReturn;
+    }
+
+    private function getFolder()
+    {
+        return implode(DIRECTORY_SEPARATOR, [self::DOMAIN, $this->aMatches['album']]);
+    }
+}
+
