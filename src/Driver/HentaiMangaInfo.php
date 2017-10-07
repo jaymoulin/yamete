e449285a4f069c669d8f90af8111c32e12f6e04f
Jay MOULIN <jaymoulin@gmail.com>
2017-10-07 10:51:30 +0200

    Added HentaiMangaInfo driver

---
+++ src/Driver/HentaiMangaInfo.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Yamete\Driver;
+
+class HentaiMangaInfo extends \Yamete\DriverAbstract
+{
+    private $aMatches = [];
+    const DOMAIN = 'hentaimanga.info';
+
+    public function canHandle()
+    {
+        return preg_match(
+            '~^https?://' . strtr(self::DOMAIN, ['.' => '\.', '-' => '\-']) . '/(?<album>[^/]+)/$~',
+            $this->sUrl,
+            $this->aMatches
+        );
+    }
+
+    public function getDownloadables()
+    {
+        $oRes = $this->getClient()->request('GET', $this->sUrl);
+        $aReturn = [];
+        $i = 0;
+        foreach ($this->getDomParser()->load((string)$oRes->getBody())->find('.hentaimanga figure a') as $oLink) {
+            /**
+             * @var \DOMElement $oLink
+             */
+            $sLink = $oLink->getAttribute('href');
+            $oRes = $this->getClient()->request('GET', $sLink);
+            $oBody = $this->getDomParser()->load((string)$oRes->getBody());
+            foreach ($oBody->find('#attachementim img') as $oImg) { //images
+                /**
+                 * @var \DOMElement $oImg
+                 */
+                $sFilename = $oImg->getAttribute('src');
+                $sBasename = $this->getFolder() . DIRECTORY_SEPARATOR . str_pad($i++, 5, '0', STR_PAD_LEFT)
+                    . '-' . basename($sFilename);
+                $aReturn[$sBasename] = $sFilename;
+            }
+        }
+        return $aReturn;
+    }
+
+    private function getFolder()
+    {
+        return implode(DIRECTORY_SEPARATOR, [self::DOMAIN, $this->aMatches['album']]);
+    }
+}
