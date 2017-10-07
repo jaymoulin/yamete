8a4c4c6b8732b71fd8572206e0da0bce601ac143
Jay MOULIN <jaymoulin@gmail.com>
2017-10-07 11:33:52 +0200

    Added Vercomicsporno driver

---
+++ src/Driver/VercomicsPorno.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Yamete\Driver;
+
+class VercomicsPorno extends \Yamete\DriverAbstract
+{
+    private $aMatches = [];
+    const DOMAIN = 'vercomicsporno.com';
+
+    public function canHandle()
+    {
+        return preg_match(
+            '~^https?://' . strtr(self::DOMAIN, ['.' => '\.', '-' => '\-']) . '/(?<album>.+)$~',
+            $this->sUrl,
+            $this->aMatches
+        );
+    }
+
+    public function getDownloadables()
+    {
+        $oRes = $this->getClient()->request('GET', $this->sUrl);
+        $aReturn = [];
+        $i = 0;
+        $bSkipFirst = true;
+        foreach ($this->getDomParser()->load((string)$oRes->getBody())->find('#posts .col-md-12 p img') as $oImg) {
+            /**
+             * @var \DOMElement $oImg
+             */
+            $sFilename = $oImg->getAttribute('data-lazy-src');
+            if ($bSkipFirst || empty($sFilename)) {
+                $bSkipFirst = false;
+                continue;
+            }
+            $sBasename = $this->getFolder() . DIRECTORY_SEPARATOR . str_pad(++$i, 5, '0', STR_PAD_LEFT)
+                . '-' . basename($sFilename);
+            $aReturn[$sBasename] = $sFilename;
+        }
+        array_pop($aReturn);
+        return $aReturn;
+    }
+
+    private function getFolder()
+    {
+        return implode(DIRECTORY_SEPARATOR, [self::DOMAIN, $this->aMatches['album']]);
+    }
+}
