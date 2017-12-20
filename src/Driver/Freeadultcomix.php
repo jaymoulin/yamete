02e341df4e541227d80862f9def94e21132a6ca2
Jay MOULIN <jaymoulin@gmail.com>
2017-12-20 02:34:04 +0100

    #4 - should fix freeadultcomix but not

--- src/Driver/Freeadultcomix.php
+++ src/Driver/Freeadultcomix.php
@@ -10,7 +10,7 @@ class Freeadultcomix extends \Yamete\DriverAbstract
     public function canHandle()
     {
         return preg_match(
-            '~^http://' . strtr(self::DOMAIN, ['.' => '\.']) . '/(?<album>[^/]+)/$~',
+            '~^https?://' . strtr(self::DOMAIN, ['.' => '\.']) . '/(?<album>[^/]+)/$~',
             $this->sUrl,
             $this->aMatches
         );
@@ -19,11 +19,10 @@ class Freeadultcomix extends \Yamete\DriverAbstract
     public function getDownloadables()
     {
         $oRes = $this->getClient()->request('GET', $this->sUrl);
-
         $aReturn = [];
         foreach ($this->getDomParser()->load((string)$oRes->getBody())->find('.single-post p img') as $oImg) {
             /** @var \DOMElement $oImg */
-            $sFilename = 'http://' . self::DOMAIN . $oImg->getAttribute('src');
+            $sFilename = $oImg->getAttribute('src');
             $aReturn[$this->getFolder() . DIRECTORY_SEPARATOR . basename($sFilename)] = $sFilename;
         }
         return $aReturn;
