46751435725ea8d1fe62aa69a6d0e7b7ab0f5f43
Jay MOULIN <jaymoulin@gmail.com>
2017-12-25 03:57:18 +0100

    Added Overwatchporn.pro

--- README.md
+++ README.md
@@ -343,5 +343,6 @@ Here's is the list of supported sites for now:
  * hentai.desi
  * xxxmilftoon.com
  * xxxcartoonpic.com
- 
+ * overwatchporn.pro
+
 You must pass the URL to the album for the program to download it!
---
+++ src/Driver/OverwatchPornPro.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Yamete\Driver;
+
+class OverwatchPornPro extends XXXComicPornCom
+{
+    const DOMAIN = 'overwatchporn.pro';
+
+    protected function getDomain(): string
+    {
+        return self::DOMAIN;
+    }
+}
