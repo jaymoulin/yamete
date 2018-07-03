0c58f5b115d8b5af18533b7197626e07fd7ba458
Jay MOULIN <jaymoulin@gmail.com>
2018-07-04 01:36:08 +0200

    added hentairead.com - to be fixed with cloudflare

---
+++ tests/Driver/HentaiRead.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace YameteTests\Driver;
+
+
+class HentaiRead extends \PHPUnit\Framework\TestCase
+{
+    public function testDownload()
+    {
+        $url = 'http://hentairead.com/ero_pippi/';
+        $driver = new \Yamete\Driver\HentaiRead();
+        $driver->setUrl($url);
+        $this->assertTrue($driver->canHandle());
+        $this->assertEquals(243, count($driver->getDownloadables()));
+    }
+}
