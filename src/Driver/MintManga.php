cc2e8981fe4bb595dfa2720c529003d77b7e38f2
Jay MOULIN <jaymoulin@gmail.com>
2018-12-26 18:27:16 +0100

    Added mintmanga.com

---
+++ src/Driver/MintManga.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Yamete\Driver;
+
+class MintManga extends \Yamete\DriverAbstract
+{
+    private $aMatches = [];
+    const DOMAIN = 'mintmanga.com';
+
+    protected function getDomain(): string
+    {
+        return self::DOMAIN;
+    }
+
+    public function canHandle(): bool
+    {
+        return (bool)preg_match(
+            '~^https?://(' . strtr($this->getDomain(), ['.' => '\.', '-' => '\-']) .
+            ')/(?<album>[^/]+)/~',
+            $this->sUrl,
+            $this->aMatches
+        );
+    }
+
+    /**
+     * @return array|string[]
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function getDownloadables(): array
+    {
+        $sBaseUrl = 'http://' . $this->getDomain();
+        $oRes = $this->getClient()->request('GET', $sBaseUrl . '/' . $this->aMatches['album'] . '/vol1/1?mtr=1');
+        $aReturn = [];
+        $i = 0;
+        foreach ($this->getDomParser()->load((string)$oRes->getBody())->find('#chapterSelectorSelect option') as $oLink) {
+            /**
+             * @var \PHPHtmlParser\Dom\AbstractNode $oLink
+             * @var \PHPHtmlParser\Dom\AbstractNode $oImg
+             */
+            $sChapterUrl = $sBaseUrl . $oLink->getAttribute('value');
+            $oRes = $this->getClient()->request('GET', $sChapterUrl . '1?mtr=1');
+            $sRegExp = '~rm_h.init\((?<json>[^\)]+)\)~';
+            if (preg_match($sRegExp, (string)$oRes->getBody(), $aMatches)) {
+                $sJsonClean = implode(',', array_slice(explode(',', trim($aMatches['json'])), 0, -2));
+                foreach (explode('],[', $sJsonClean) as $sString) {
+                    $sFilename = str_replace(['"', '\''], '', implode('', array_slice(explode(',', $sString), 1, 2)));
+                    $sBasename = $this->getFolder() . DIRECTORY_SEPARATOR . str_pad($i++, 5, '0', STR_PAD_LEFT)
+                        . '-' . basename($sFilename);
+                    $aReturn[$sBasename] = $sFilename;
+                }
+            }
+        }
+        return $aReturn;
+    }
+
+    private function getFolder(): string
+    {
+        return implode(DIRECTORY_SEPARATOR, [$this->getDomain(), $this->aMatches['album']]);
+    }
+}
---
+++ tests/Driver/MintManga.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace YameteTests\Driver;
+
+
+class MintManga extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function testDownload()
+    {
+        $url = 'http://mintmanga.com/jagaaaaaan/';
+        $driver = new \Yamete\Driver\MintManga();
+        $driver->setUrl($url);
+        $this->assertTrue($driver->canHandle());
+        $this->assertEquals(846, count($driver->getDownloadables()));
+    }
+}
