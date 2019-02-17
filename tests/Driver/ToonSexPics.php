464313a2aeace1a93030a2f1fc778f932b5276d2
Jay MOULIN <jaymoulin@gmail.com>
2019-02-17 16:44:36 +0100

    Fixes ToonSexPics

--- tests/Driver/ToonSexPics.php
+++ tests/Driver/ToonSexPics.php
@@ -14,6 +14,6 @@ class ToonSexPics extends \PHPUnit\Framework\TestCase
         $driver = new \Yamete\Driver\ToonSexPics();
         $driver->setUrl($url);
         $this->assertTrue($driver->canHandle());
-        $this->assertEquals(15, count($driver->getDownloadables()));
+        $this->assertEquals(387, count($driver->getDownloadables()));
     }
 }
