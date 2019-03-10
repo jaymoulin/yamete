9c7905afc678c945747d4abbb73c474f27751584
Jay MOULIN <jaymoulin@gmail.com>
2019-03-10 20:12:04 +0100

    added superhentais.com

--- README.md
+++ README.md
@@ -241,5 +241,6 @@ Here's is the list of supported sites for now:
  * manhwahentai.com
  * hentaiyaro.com
  * hentai-id.tv
+ * superhentais.com
 
 You must pass the URL to the album for the program to download it!
---
+++ tests/Driver/SuperHentaisCom.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace YameteTests\Driver;
+
+
+class SuperHentaisCom extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function testDownload()
+    {
+        $url = 'https://www.superhentais.com/parody-hentai-manga/slutty-misty/6078363';
+        $driver = new \Yamete\Driver\SuperHentaisCom();
+        $driver->setUrl($url);
+        $this->assertTrue($driver->canHandle());
+        $this->assertEquals(7, count($driver->getDownloadables()));
+    }
+}
