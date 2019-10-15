a217a10bfdf7510665260b08f8c1732050c846d2
Jay MOULIN <jaymoulin@gmail.com>
2019-10-15 18:14:49 +0200

    Fixes tests page number

--- tests/Driver/MangaKakalot.php
+++ tests/Driver/MangaKakalot.php
@@ -14,7 +14,7 @@ class MangaKakalot extends \PHPUnit\Framework\TestCase
         $driver = new \Yamete\Driver\MangaKakalot();
         $driver->setUrl($url);
         $this->assertTrue($driver->canHandle());
-        $this->assertEquals(607, count($driver->getDownloadables()));
+        $this->assertEquals(621, count($driver->getDownloadables()));
     }
 
     /**
