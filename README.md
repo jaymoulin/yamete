8b1bc539ef297135d83bf21323959cbe5259f763
Jay MOULIN <jaymoulin@gmail.com>
2019-11-16 01:06:19 +0100

    Adds kingsmanga.net

--- README.md
+++ README.md
@@ -259,5 +259,6 @@ Here's is the list of supported sites for now:
  * g6hentai.com
  * naughtyhentai.com
  * hentai.tv
+ * kingsmanga.net
 
 You must pass the URL to the album for the program to download it!
---
+++ tests/Driver/KingsMangaNet.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace YameteTests\Driver;
+
+
+class KingsMangaNet extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function testDownload()
+    {
+        $url = 'https://www.kingsmanga.net/manga/chronicles-of-heavenly-demon/';
+        $driver = new \Yamete\Driver\KingsMangaNet();
+        $driver->setUrl($url);
+        $this->assertTrue($driver->canHandle());
+        $this->assertEquals(729, count($driver->getDownloadables()));
+    }
+}
