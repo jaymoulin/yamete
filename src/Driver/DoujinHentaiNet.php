c1fa95e27c65dbe58cde00c470d711c5f750067c
Jay MOULIN <jaymoulin@gmail.com>
2020-01-06 03:27:00 +0100

    Fixes doujinhentai.net

--- src/Driver/DoujinHentaiNet.php
+++ src/Driver/DoujinHentaiNet.php
@@ -2,6 +2,9 @@
 
 namespace Yamete\Driver;
 
+use GuzzleCloudflare\Middleware;
+use GuzzleHttp\Cookie\FileCookieJar;
+
 class DoujinHentaiNet extends \Yamete\DriverAbstract
 {
     private $aMatches = [];
@@ -55,5 +58,26 @@ class DoujinHentaiNet extends \Yamete\DriverAbstract
     private function getFolder(): string
     {
         return implode(DIRECTORY_SEPARATOR, [$this->getDomain(), $this->aMatches['album']]);
+    }
+
+    /**
+     * @param array $aOptions
+     * @return \GuzzleHttp\Client
+     */
+    public function getClient(array $aOptions = []): \GuzzleHttp\Client
+    {
+        $oClient = parent::getClient(
+            [
+                'cookies' => new FileCookieJar(tempnam('/tmp', __CLASS__)),
+                'headers' => ['User-Agent' => self::USER_AGENT],
+            ]
+        );
+        /**
+         * @var \GuzzleHttp\HandlerStack $oHandler
+         */
+        $oHandler = $oClient->getConfig('handler');
+        $oHandler->remove('cloudflare');
+        $oHandler->push(Middleware::create(), 'cloudflare');
+        return $oClient;
     }
 }
