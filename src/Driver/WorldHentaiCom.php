8de1eab181f2ec26e9a006a2b9ad22c776172603
Jay MOULIN <jaymoulin@gmail.com>
2020-01-12 03:33:49 +0100

    Fixes worldhentai.com

--- src/Driver/WorldHentaiCom.php
+++ src/Driver/WorldHentaiCom.php
@@ -32,8 +32,13 @@ class WorldHentaiCom extends \Yamete\DriverAbstract
         $oPages = $this->getDomParser()->load((string)$oRes->getBody())->find('a.highslide');
         $index = 0;
         $aReturn = [];
+        $aDeduplicate = [];
         foreach ($oPages as $oLink) {
             $sFilename = $oLink->getAttribute('href');
+            if (isset($aDeduplicate[$sFilename])) {
+                continue;
+            }
+            $aDeduplicate[$sFilename] = true;
             $sBasename = $this->getFolder() . DIRECTORY_SEPARATOR . str_pad($index++, 5, '0', STR_PAD_LEFT)
                 . '-' . basename($sFilename);
             $aReturn[$sBasename] = $sFilename;
