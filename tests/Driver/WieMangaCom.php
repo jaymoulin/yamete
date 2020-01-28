0c5ca47b2b1f05decd8bae2fcdac3cb0975d111a
Jay MOULIN <jaymoulin@gmail.com>
2020-01-28 14:59:54 +0100

    Adds wiemanga.com

---
+++ tests/Driver/WieMangaCom.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace YameteTests\Driver;
+
+
+class WieMangaComCom extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function testDownload()
+    {
+        $url = 'https://www.wiemanga.com/manga/Momo_no_Musume.html';
+        $driver = new \Yamete\Driver\WieMangaCom();
+        $driver->setUrl($url);
+        $this->assertTrue($driver->canHandle());
+        $this->assertEquals(88, count($driver->getDownloadables()));
+    }
+}
