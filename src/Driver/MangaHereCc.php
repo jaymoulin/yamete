d6b18b395e9b481ee08b82332d4eb92e1af58135
Jay MOULIN <jaymoulin@gmail.com>
2020-02-29 22:09:38 +0100

    fixes mangahere.cc

--- src/Driver/MangaHereCc.php
+++ src/Driver/MangaHereCc.php
@@ -4,83 +4,85 @@ namespace Yamete\Driver;
 
 use Yamete\DriverAbstract;
 
-class MangaHereCc extends DriverAbstract
-{
-    private $aMatches = [];
-    const DOMAIN = 'mangahere.cc';
-
-    public function canHandle(): bool
+if (!class_exists(MangaHereCc::class)) {
+    class MangaHereCc extends DriverAbstract
     {
-        return (bool)preg_match(
-            '~^https?://(www\.)?(' . strtr($this->getDomain(), ['.' => '\.']) . ')/manga/(?<album>[^/]+)~',
-            $this->sUrl,
-            $this->aMatches
-        );
-    }
+        private $aMatches = [];
+        const DOMAIN = 'mangahere.cc';
 
-    /**
-     * @return string
-     */
-    protected function getDomain(): string
-    {
-        return self::DOMAIN;
-    }
+        public function canHandle(): bool
+        {
+            return (bool)preg_match(
+                '~^https?://(www\.)?(' . strtr($this->getDomain(), ['.' => '\.']) . ')/manga/(?<album>[^/]+)~',
+                $this->sUrl,
+                $this->aMatches
+            );
+        }
 
-
-    public function getDownloadables(): array
-    {
         /**
-         * @var \iterator $oChapters
-         * @var \PHPHtmlParser\Dom\AbstractNode[] $aChapters
-         * @var \PHPHtmlParser\Dom\AbstractNode[] $oPages
-         * @var \PHPHtmlParser\Dom\AbstractNode $oImage
+         * @return string
          */
-        $sStartUrl = 'https://' . $this->getDomain();
-        $sUrl = $sStartUrl . '/manga/' . $this->aMatches['album'] . '/';
-        $oResponse = $this->getClient()->get($sUrl);
-        $oChapters = $this->getDomParser()->load((string)$oResponse->getBody())->find('.detail-main-list li > a');
-        $aChapters = iterator_to_array($oChapters);
-        krsort($aChapters);
-        $index = 0;
-        $aReturn = [];
-        foreach ($aChapters as $oLink) {
-            $sCurrentUrl = $sStartUrl . $oLink->getAttribute('href');
-            if (false === strpos($sCurrentUrl, '/1.html')) {
-                continue;
-            }
-            $oResponse = $this->getClient()->get($sCurrentUrl);
-            $iPageCount = 0;
-            $oPages = $this->getDomParser()->load((string)$oResponse->getBody())->find('.pager-list-left a');
-            foreach ($oPages as $oPage) {
-                $iCurrentPage = $oPage->getAttribute('data-page');
-                $iPageCount = $iCurrentPage >= $iPageCount ? $iCurrentPage : $iPageCount;
-            }
-            for ($iCurrentPage = 1; $iCurrentPage <= $iPageCount; $iCurrentPage++) {
-                $oResponse = $this->getClient()->get(str_replace('/1.html', "/$iCurrentPage.html", $sCurrentUrl));
-                $oImage = $this->getDomParser()->load((string)$oResponse->getBody())->find('.reader-main-img')[0];
-                if (!$oImage) {
+        protected function getDomain(): string
+        {
+            return self::DOMAIN;
+        }
+
+
+        public function getDownloadables(): array
+        {
+            /**
+             * @var \iterator $oChapters
+             * @var \PHPHtmlParser\Dom\AbstractNode[] $aChapters
+             * @var \PHPHtmlParser\Dom\AbstractNode[] $oPages
+             * @var \PHPHtmlParser\Dom\AbstractNode $oImage
+             */
+            $sStartUrl = 'https://' . $this->getDomain();
+            $sUrl = $sStartUrl . '/manga/' . $this->aMatches['album'] . '/';
+            $oResponse = $this->getClient()->get($sUrl);
+            $oChapters = $this->getDomParser()->load((string)$oResponse->getBody())->find('.detail-main-list li > a');
+            $aChapters = iterator_to_array($oChapters);
+            krsort($aChapters);
+            $index = 0;
+            $aReturn = [];
+            foreach ($aChapters as $oLink) {
+                $sCurrentUrl = $sStartUrl . $oLink->getAttribute('href');
+                if (false === strpos($sCurrentUrl, '/1.html')) {
                     continue;
                 }
-                $sFilename = 'https:' . $oImage->getAttribute('src');
-                $sBasename = $this->getFolder() . DIRECTORY_SEPARATOR . str_pad($index++, 5, '0', STR_PAD_LEFT)
-                    . '-' . basename($sFilename);
-                $aReturn[$sBasename] = $sFilename;
+                $oResponse = $this->getClient()->get($sCurrentUrl);
+                $iPageCount = 0;
+                $oPages = $this->getDomParser()->load((string)$oResponse->getBody())->find('.pager-list-left a');
+                foreach ($oPages as $oPage) {
+                    $iCurrentPage = $oPage->getAttribute('data-page');
+                    $iPageCount = $iCurrentPage >= $iPageCount ? $iCurrentPage : $iPageCount;
+                }
+                for ($iCurrentPage = 1; $iCurrentPage <= $iPageCount; $iCurrentPage++) {
+                    $oResponse = $this->getClient()->get(str_replace('/1.html', "/$iCurrentPage.html", $sCurrentUrl));
+                    $oImage = $this->getDomParser()->load((string)$oResponse->getBody())->find('.reader-main-img')[0];
+                    if (!$oImage) {
+                        continue;
+                    }
+                    $sFilename = 'https:' . $oImage->getAttribute('src');
+                    $sBasename = $this->getFolder() . DIRECTORY_SEPARATOR . str_pad($index++, 5, '0', STR_PAD_LEFT)
+                        . '-' . basename($sFilename);
+                    $aReturn[$sBasename] = $sFilename;
+                }
             }
+            return $aReturn;
         }
-        return $aReturn;
-    }
 
-    private function getFolder(): string
-    {
-        return implode(DIRECTORY_SEPARATOR, [$this->getDomain(), $this->aMatches['album']]);
-    }
+        private function getFolder(): string
+        {
+            return implode(DIRECTORY_SEPARATOR, [$this->getDomain(), $this->aMatches['album']]);
+        }
 
-    public function getClient(array $aOptions = []): \GuzzleHttp\Client
-    {
-        return parent::getClient(
-            [
-                'headers' => ['Cookie' => 'isAdult=1'],
-            ]
-        );
+        public function getClient(array $aOptions = []): \GuzzleHttp\Client
+        {
+            return parent::getClient(
+                [
+                    'headers' => ['Cookie' => 'isAdult=1'],
+                ]
+            );
+        }
     }
 }
