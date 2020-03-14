0db65205e1eb86c81a7c64411d528b7f6c57a8af
Jay MOULIN <jaymoulin@gmail.com>
2020-03-14 19:01:52 +0100

    Optimize hitomi.la driver

--- src/Driver/Hitomi.php
+++ src/Driver/Hitomi.php
@@ -34,9 +34,9 @@ class Hitomi extends \Yamete\DriverAbstract
         if (!isset($aItems['files'])) {
             return [];
         }
+        $cChosenCdn = null;
         foreach ($aItems['files'] as $aItem) {
             foreach (['a', 'b', 'c'] as $cCdn) {
-
                 /**
                  * @var \PHPHtmlParser\Dom\HtmlNode $oImg
                  */
@@ -46,11 +46,15 @@ class Hitomi extends \Yamete\DriverAbstract
                 $bHasWebp = isset($aItem['haswebp']) && $aItem['haswebp'];
                 $sCategory = $bHasWebp ? 'webp' : 'images';
                 $sExt = $bHasWebp ? 'webp' : substr($aItem['name'], strrpos($aItem['name'], '.') + 1);
-                $sFilename = "https://${cCdn}a." . self::DOMAIN . "/$sCategory/$sFolder/$sSubFolder/${sHash}.$sExt";
-                $oRes = $this->getClient()->request('GET', $sFilename, ["http_errors" => false]);
-                if ($oRes->getStatusCode() !== 200) {
-                    continue;
+                $cNowCdn = $cChosenCdn !== null ? $cChosenCdn : $cCdn;
+                $sFilename = "https://${cNowCdn}a." . self::DOMAIN . "/$sCategory/$sFolder/$sSubFolder/${sHash}.$sExt";
+                if ($cChosenCdn === null) {
+                    $oRes = $this->getClient()->request('GET', $sFilename, ["http_errors" => false]);
+                    if ($oRes->getStatusCode() !== 200) {
+                        continue;
+                    }
                 }
+                $cChosenCdn = $cCdn;
                 $sBasename = $this->getFolder() . DIRECTORY_SEPARATOR . str_pad(++$index, 5, '0', STR_PAD_LEFT)
                     . '-' . basename($sFilename);
                 $aReturn[$sBasename] = $sFilename;
