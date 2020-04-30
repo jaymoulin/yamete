d97fae1d1032c851bb308ccbd93956b6352eba1d
Jay MOULIN <jaymoulin@gmail.com>
2020-04-30 22:19:05 +0200

    Fixes bato.to

--- src/Driver/BatoTo.php
+++ src/Driver/BatoTo.php
@@ -2,8 +2,8 @@
 
 namespace Yamete\Driver;
 
+use GuzzleCloudflare\Middleware;
 use GuzzleHttp\Cookie\FileCookieJar;
-use GuzzleHttp\Client;
 
 class BatoTo extends \Yamete\DriverAbstract
 {
@@ -58,5 +58,26 @@ class BatoTo extends \Yamete\DriverAbstract
             }
         }
         return $aReturn;
+    }
+
+    /**
+     * @param array $aOptions
+     * @return \GuzzleHttp\Client
+     */
+    public function getClient(array $aOptions = []): \GuzzleHttp\Client
+    {
+        $oClient = parent::getClient(
+            [
+                'cookies' => new FileCookieJar(tempnam('/tmp', __CLASS__)),
+                'headers' => ['User-Agent' => self::USER_AGENT],
+            ]
+        );
+        /**
+         * @var \GuzzleHttp\HandlerStack $oHandler
+         */
+        $oHandler = $oClient->getConfig('handler');
+        $oHandler->remove('cloudflare');
+        $oHandler->push(Middleware::create(), 'cloudflare');
+        return $oClient;
     }
 }
