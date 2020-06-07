7ff2ba41379754282a7049b1a6bb5d81a3a7df40
Jay MOULIN <jaymoulin@gmail.com>
2020-06-07 19:49:41 +0200

    removes middleware for hentai2read.com

--- src/Driver/Hentai2read.php
+++ src/Driver/Hentai2read.php
@@ -2,9 +2,6 @@
 
 namespace Yamete\Driver;
 
-use \GuzzleCloudflare\Middleware;
-use \GuzzleHttp\Cookie\FileCookieJar;
-
 class Hentai2read extends \Yamete\DriverAbstract
 {
     private $aMatches = [];
@@ -19,27 +16,6 @@ class Hentai2read extends \Yamete\DriverAbstract
             $this->sUrl,
             $this->aMatches
         );
-    }
-
-    /**
-     * @param array $aOptions
-     * @return \GuzzleHttp\Client
-     */
-    public function getClient(array $aOptions = []): \GuzzleHttp\Client
-    {
-        $oClient = parent::getClient(
-            [
-                'cookies' => new FileCookieJar(tempnam('/tmp', __CLASS__)),
-                'headers' => ['User-Agent' => self::USER_AGENT],
-            ]
-        );
-        /**
-         * @var \GuzzleHttp\HandlerStack $oHandler
-         */
-        $oHandler = $oClient->getConfig('handler');
-        $oHandler->remove('cloudflare');
-        $oHandler->push(Middleware::create(), 'cloudflare');
-        return $oClient;
     }
 
     /**
