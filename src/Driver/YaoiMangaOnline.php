02cf821805c5d4d852e67fadcf59ac4faeb3f19f
Jay MOULIN <jaymoulin@gmail.com>
2020-06-07 20:51:34 +0200

    removes middleware for yaoimangaonline.com

--- src/Driver/YaoiMangaOnline.php
+++ src/Driver/YaoiMangaOnline.php
@@ -2,9 +2,6 @@
 
 namespace Yamete\Driver;
 
-use \GuzzleCloudflare\Middleware;
-use \GuzzleHttp\Cookie\FileCookieJar;
-
 class YaoiMangaOnline extends \Yamete\DriverAbstract
 {
     private $aMatches = [];
@@ -39,29 +36,6 @@ class YaoiMangaOnline extends \Yamete\DriverAbstract
         }
         return $aReturn;
     }
-
-
-    /**
-     * @param array $aOptions
-     * @return \GuzzleHttp\Client
-     */
-    public function getClient(array $aOptions = []): \GuzzleHttp\Client
-    {
-        $oClient = parent::getClient(
-            [
-                'cookies' => new FileCookieJar(tempnam('/tmp', __CLASS__)),
-                'headers' => ['User-Agent' => self::USER_AGENT],
-            ]
-        );
-        /**
-         * @var \GuzzleHttp\HandlerStack $oHandler
-         */
-        $oHandler = $oClient->getConfig('handler');
-        $oHandler->remove('cloudflare');
-        $oHandler->push(Middleware::create(), 'cloudflare');
-        return $oClient;
-    }
-
 
     private function getFolder(): string
     {
