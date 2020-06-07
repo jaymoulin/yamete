3c5e41a5d1e6c830b7b7654793fafbb8033e9e64
Jay MOULIN <jaymoulin@gmail.com>
2020-06-07 20:23:00 +0200

    removes middleware for mangarockteam.com

--- src/Driver/MangaRockTeamCom.php
+++ src/Driver/MangaRockTeamCom.php
@@ -2,9 +2,6 @@
 
 namespace Yamete\Driver;
 
-use GuzzleCloudflare\Middleware;
-use GuzzleHttp\Cookie\FileCookieJar;
-
 class MangaRockTeamCom extends IsekaiScanCom
 {
     const DOMAIN = 'mangarockteam.com';
@@ -12,26 +9,5 @@ class MangaRockTeamCom extends IsekaiScanCom
     protected function getDomain(): string
     {
         return self::DOMAIN;
-    }
-
-    /**
-     * @param array $aOptions
-     * @return \GuzzleHttp\Client
-     */
-    public function getClient(array $aOptions = []): \GuzzleHttp\Client
-    {
-        $oClient = parent::getClient(
-            [
-                'cookies' => new FileCookieJar(tempnam('/tmp', __CLASS__)),
-                'headers' => ['User-Agent' => self::USER_AGENT],
-            ]
-        );
-        /**
-         * @var \GuzzleHttp\HandlerStack $oHandler
-         */
-        $oHandler = $oClient->getConfig('handler');
-        $oHandler->remove('cloudflare');
-        $oHandler->push(Middleware::create(), 'cloudflare');
-        return $oClient;
     }
 }
