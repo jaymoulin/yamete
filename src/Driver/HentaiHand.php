dafb9ddaaecc7f5d90a6ef65007e985c39708f96
Jay MOULIN <jaymoulin@gmail.com>
2020-09-19 02:07:53 +0200

    Fixes hentaihand.com

--- src/Driver/HentaiHand.php
+++ src/Driver/HentaiHand.php
@@ -26,14 +26,13 @@ class HentaiHand extends DriverAbstract
      */
     public function getDownloadables(): array
     {
-        $sUrl = 'https://' . self::DOMAIN . '/api/comics/' . $this->aMatches['album'] . '?nsfw=true';
+        $sUrl = 'https://' . self::DOMAIN . '/api/comics/' . $this->aMatches['album'] . '/images?nsfw=true';
         $oRes = $this->getClient()->request('GET', $sUrl);
         $aInfos = \GuzzleHttp\json_decode((string)$oRes->getBody(), true);
         $aReturn = [];
         $index = 0;
-        $iNbPages = $aInfos['pages'];
-        for ($iPage = 1; $iPage <= $iNbPages; $iPage++) {
-            $sFilename = 'https://cdn.' . self::DOMAIN . "/assets/thumbnails/${aInfos['id']}/${iPage}.png";
+        foreach ($aInfos['images'] as $aData) {
+            $sFilename = $aData['source_url'];
             $sBasename = $this->getFolder() . DIRECTORY_SEPARATOR . str_pad($index++, 5, '0', STR_PAD_LEFT)
                 . '-' . basename($sFilename);
             $aReturn[$sBasename] = $sFilename;
