95f7942d97f5b509f886f97cee8a94ff3e2bde54
Jay MOULIN <jaymoulin@gmail.com>
2020-10-26 23:51:18 +0100

    Fixes mangahentai.me

--- src/Driver/MangaHentaiMe.php
+++ src/Driver/MangaHentaiMe.php
@@ -7,65 +7,77 @@ use PHPHtmlParser\Dom\AbstractNode;
 use Traversable;
 use Yamete\DriverAbstract;
 
-if (!class_exists(MangaHentaiMe::class)) {
-    class MangaHentaiMe extends DriverAbstract
+class MangaHentaiMe extends DriverAbstract
+{
+    const DOMAIN = 'mangahentai.me';
+    protected $aMatches = [];
+
+    protected function getDomain(): string
     {
-        const DOMAIN = 'mangahentai.me';
-        protected $aMatches = [];
+        return self::DOMAIN;
+    }
 
-        protected function getDomain(): string
-        {
-            return self::DOMAIN;
-        }
+    public function canHandle(): bool
+    {
+        $sReg = '~^https?://(' . strtr($this->getDomain(), ['.' => '\.']) . ')/(?<category>[^/]+)/(?<album>[^/]+)~';
+        return (bool)preg_match($sReg, $this->sUrl, $this->aMatches);
+    }
 
-        public function canHandle(): bool
-        {
-            $sReg = '~^https?://(' . strtr($this->getDomain(), ['.' => '\.']) . ')/(?<category>[^/]+)/(?<album>[^/]+)~';
-            return (bool)preg_match($sReg, $this->sUrl, $this->aMatches);
-        }
-
+    /**
+     * @return array|string[]
+     * @throws GuzzleException
+     */
+    public function getDownloadables(): array
+    {
         /**
-         * @return array|string[]
-         * @throws GuzzleException
+         * @var Traversable $oChapters
+         * @var AbstractNode $oChapter
+         * @var AbstractNode $oImg
          */
-        public function getDownloadables(): array
-        {
-            /**
-             * @var Traversable $oChapters
-             * @var AbstractNode $oChapter
-             * @var AbstractNode $oImg
-             */
-            $sUrl = 'https://' . $this->getDomain() . '/' . $this->aMatches['category']
-                . '/' . $this->aMatches['album'] . '/';
-            $oRes = $this->getClient()->request('GET', $sUrl);
+        $sUrl = 'https://' . $this->getDomain() . '/' . $this->aMatches['category']
+            . '/' . $this->aMatches['album'] . '/';
+        $oRes = $this->getClient()->request('GET', $sUrl);
+        $aMatches = [];
+        if (!preg_match('~"manga_id":"([0-9]+)"~', (string)$oRes->getBody(), $aMatches)) {
+            return [];
+        }
+        $sResponse = (string)$this->getClient()
+            ->request(
+                'POST',
+                'https://' . self::DOMAIN . '/wp-admin/admin-ajax.php',
+                [
+                    'headers' => [
+                        'X-Requested-With' => 'XMLHttpRequest',
+                    ],
+                    'form_params' => [
+                        'action' => 'manga_get_chapters',
+                        'manga' => $aMatches[1],
+                    ],
+                ]
+            )->getBody();
+        $oChapters = $this->getDomParser()->load($sResponse)->find('.wp-manga-chapter a');
+        $aChapters = iterator_to_array($oChapters);
+        krsort($aChapters);
+        $aReturn = [];
+        $index = 0;
+        foreach ($aChapters as $oChapter) {
+            $oRes = $this->getClient()->request('GET', $oChapter->getAttribute('href'));
             $aMatches = [];
-            $sRegExp = '~<li class="wp-manga-chapter[^"]+">[^<]+<a href="([^"]+)~us';
-            if (!preg_match_all($sRegExp, (string)$oRes->getBody(), $aMatches)) {
-                return [];
+            if (!preg_match_all('~src="([^"]+)" class="wp-manga-chapter-img~', (string)$oRes->getBody(), $aMatches)) {
+                continue;
             }
-            $aChapters = $aMatches[1];
-            krsort($aChapters);
-            $index = 0;
-            $aReturn = [];
-            foreach ($aChapters as $sChapter) {
-                $oRes = $this->getClient()->request('GET', $sChapter);
-                $aMatches = [];
-                if (!preg_match_all('~src="([^"]+)" class="wp-manga-chapter-img~', (string)$oRes->getBody(), $aMatches)) {
-                    continue;
-                }
-                foreach ($aMatches[1] as $sFilename) {
-                    $sFilename = trim($sFilename);
-                    $sBasename = $this->getFolder() . DIRECTORY_SEPARATOR . str_pad($index++, 5, '0', STR_PAD_LEFT)
-                        . '-' . basename($sFilename);
-                    $aReturn[$sBasename] = $sFilename;
-                }
+            foreach ($aMatches[1] as $sFilename) {
+                $sFilename = trim($sFilename);
+                $sBasename = $this->getFolder() . DIRECTORY_SEPARATOR . str_pad($index++, 5, '0', STR_PAD_LEFT)
+                    . '-' . basename($sFilename);
+                $aReturn[$sBasename] = $sFilename;
             }
-            return $aReturn;
         }
+        return $aReturn;
+    }
 
-        private function getFolder(): string
-        {
-            return implode(DIRECTORY_SEPARATOR, [$this->getDomain(), $this->aMatches['album']]);
-        }
+    private function getFolder(): string
+    {
+        return implode(DIRECTORY_SEPARATOR, [$this->getDomain(), $this->aMatches['album']]);
     }
 }
