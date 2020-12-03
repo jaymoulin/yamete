1fd1b529c5b26983fa7604b65250f11aab958529
Jay MOULIN <jaymoulin@gmail.com>
2020-12-03 02:38:42 +0100

    Fixes manganeloteam.com

--- src/Driver/ManganeloTeamCom.php
+++ src/Driver/ManganeloTeamCom.php
@@ -2,22 +2,111 @@
 
 namespace Yamete\Driver;
 
+use GuzzleHttp\Client;
+use PHPHtmlParser\Dom\AbstractNode;
+use GuzzleHttp\Exception\GuzzleException;
+
 class ManganeloTeamCom extends IsekaiScanCom
 {
     private const DOMAIN = 'manganeloteam.com';
 
+    protected $aMatches = [];
+
+    public function canHandle(): bool
+    {
+        return (bool)preg_match(
+            '~^https?://(' . strtr($this->getDomain(), ['.' => '\.']) . ')/manga/(?<album>[^/]+)~',
+            $this->sUrl,
+            $this->aMatches
+        );
+    }
+
+    /**
+     * @return string
+     */
     protected function getDomain(): string
     {
         return self::DOMAIN;
     }
 
-    protected function getRegexp(): string
+    /**
+     * Where to download
+     * @return string
+     */
+    protected function getFolder(): string
     {
-        return '~src="([^"]+)" class="wp-manga-chapter-img~';
+        return implode(DIRECTORY_SEPARATOR, [$this->getDomain(), $this->aMatches['album']]);
     }
 
+    /**
+     * Rule to get all chapters links
+     * @return string
+     */
     protected function getChapterRule(): string
     {
-        return '.wp-manga-chapter a';
+        return '.chapter a';
+    }
+
+    /**
+     * @return array|string[]
+     * @throws GuzzleException
+     */
+    public function getDownloadables(): array
+    {
+        /**
+         * @var \Traversable $oChapters
+         * @var AbstractNode $oChapter
+         * @var AbstractNode $oImg
+         */
+        $sUrl = 'https://' . $this->getDomain() . '/' . $this->aMatches['category']
+            . '/' . $this->aMatches['album'] . '/';
+        $oRes = $this->getClient()->request('GET', $sUrl);
+        $aMatches = [];
+        if (!preg_match('~"manga_id":"([0-9]+)"~', (string)$oRes->getBody(), $aMatches)) {
+            return [];
+        }
+        $sResponse = (string)$this->getClient()
+            ->request(
+                'POST',
+                'https://' . self::DOMAIN . '/wp-admin/admin-ajax.php',
+                [
+                    'headers' => [
+                        'X-Requested-With' => 'XMLHttpRequest',
+                    ],
+                    'form_params' => [
+                        'action' => 'manga_get_chapters',
+                        'manga' => $aMatches[1],
+                    ],
+                ]
+            )->getBody();
+        $oChapters = $this->getDomParser()->load($sResponse)->find('.wp-manga-chapter a');
+        $aChapters = iterator_to_array($oChapters);
+        krsort($aChapters);
+        $aReturn = [];
+        $index = 0;
+        foreach ($aChapters as $oChapter) {
+            $oRes = $this->getClient()->request('GET', $oChapter->getAttribute('href'));
+            $aMatches = [];
+            if (!preg_match_all('~src="([^"]+)" class="wp-manga-chapter-img~', (string)$oRes->getBody(), $aMatches)) {
+                continue;
+            }
+            foreach ($aMatches[1] as $sFilename) {
+                $sFilename = trim($sFilename);
+                $sBasename = $this->getFolder() . DIRECTORY_SEPARATOR . str_pad($index++, 5, '0', STR_PAD_LEFT)
+                    . '-' . basename($sFilename);
+                $aReturn[$sBasename] = $sFilename;
+            }
+        }
+        return $aReturn;
+    }
+
+    protected function getRegexp(): string
+    {
+        return '~data-src="([^"]+)" class="wp-manga~';
+    }
+
+    public function getClient(array $aOptions = []): Client
+    {
+        return parent::getClient(['headers' => ['Referer' => $this->sUrl]]);
     }
 }
