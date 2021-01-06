c31087215dc1741d6890d840ccf57a01a85305fb
Jay MOULIN <jaymoulin@gmail.com>
2021-01-06 18:45:45 +0100

    Fixes hentainexus.com

--- src/Driver/HentaiNexusCom.php
+++ src/Driver/HentaiNexusCom.php
@@ -15,7 +15,7 @@ class HentaiNexusCom extends DriverAbstract
     public function canHandle(): bool
     {
         return (bool)preg_match(
-            '~^https?://(' . strtr(self::DOMAIN, ['.' => '\.']) . ')/view/(?<album>[0-9]+)$~',
+            '~^https?://(' . strtr(self::DOMAIN, ['.' => '\.']) . ')/(view|read)/(?<album>[0-9]+)~',
             $this->sUrl,
             $this->aMatches
         );
@@ -38,24 +38,69 @@ class HentaiNexusCom extends DriverAbstract
     {
         /**
          * @var AbstractNode $oImg
-         * @var AbstractNode[] $oPages
          */
+        $this->sUrl = 'https://' . self::DOMAIN . '/read/' . $this->aMatches['album'];
         $oResult = $this->getClient()->request('GET', $this->sUrl);
-        $oPages = $this->getDomParser()->load((string)$oResult->getBody())->find('.columns a');
+        $aMatches = [];
+        if (!preg_match('~initReader\("([^"]+)~', (string)$oResult->getBody(), $aMatches)) {
+            return [];
+        }
+        $aJson = $this->getJson($aMatches[1]);
         $index = 0;
         $aReturn = [];
-        foreach ($oPages as $oPage) {
-            $sUrl = 'https://' . self::DOMAIN . $oPage->getAttribute('href');
-            if (!preg_match('~/read/([0-9]+)/([0-9]+)~', $sUrl)) {
-                continue;
-            }
-            $oResult = $this->getClient()->request('GET', $sUrl);
-            $oImg = $this->getDomParser()->load((string)$oResult->getBody())->find('#currImage')[0];
-            $sFilename = $oImg->getAttribute('src');
+        foreach ($aJson['pages'] as $sFilename) {
+            $sFilename = substr($sFilename, 0, strpos($sFilename, '?'));
             $sBasename = $this->getFolder() . DIRECTORY_SEPARATOR . str_pad($index++, 5, '0', STR_PAD_LEFT)
                 . '-' . basename($sFilename);
             $aReturn[$sBasename] = $sFilename;
         }
         return $aReturn;
+    }
+
+    /**
+     * Get assets from initReader
+     * @param string $sToDecode
+     * @return array
+     */
+    private function getJson(string $sToDecode): array
+    {
+        $baseDecoded = base64_decode($sToDecode);
+        $baseDecodedLength = strlen($baseDecoded);
+        $_0x204508 = 0x0;
+        $_0x3102fa = [];
+        $_0x3e2b5d = [];
+        for ($tabOfKey = 0x2; count($_0x3e2b5d) < 0x10; ++$tabOfKey) {
+            if (!$_0x3102fa[$tabOfKey]) {
+                $_0x3e2b5d[] = $tabOfKey;
+                for ($_0x1a7e37 = $tabOfKey << 0x1; $_0x1a7e37 <= 256; $_0x1a7e37 += $tabOfKey) $_0x3102fa[$_0x1a7e37] = true;
+            }
+        }
+
+        for ($tabOfKey = 0x0; $tabOfKey < 64; $tabOfKey++) {
+            $_0x204508 = $_0x204508 ^ ord($baseDecoded[$tabOfKey]);
+            for ($_0x1a7e37 = 0x0; $_0x1a7e37 < 0x8; $_0x1a7e37++) $_0x204508 = $_0x204508 & 0x1 ? $_0x204508 >> 0x1 ^ 0xc : $_0x204508 >> 0x1;
+        }
+        $_0x204508 = $_0x204508 & 0x7;
+
+        for ($tabOfKey = [], $_0x17bc47 = 0x0, $jsonToParse = '', $_0x47a1c9 = 0x0; $_0x47a1c9 < 256; $_0x47a1c9++) {
+            $tabOfKey[$_0x47a1c9] = $_0x47a1c9;
+        }
+        for ($_0x47a1c9 = 0x0; $_0x47a1c9 < 256; $_0x47a1c9++) {
+            $_0x17bc47 = ($_0x17bc47 + $tabOfKey[$_0x47a1c9] + ord($baseDecoded[$_0x47a1c9 % 64])) % 256;
+            $_0x734e1c = $tabOfKey[$_0x47a1c9];
+            $tabOfKey[$_0x47a1c9] = $tabOfKey[$_0x17bc47];
+            $tabOfKey[$_0x17bc47] = $_0x734e1c;
+        }
+        for ($_0x517f5a = $_0x3e2b5d[$_0x204508], $_0xcd3dde = 0x0, $_0x206008 = 0x0, $_0x47a1c9 = 0x0, $_0x17bc47 = 0x0, $charAt64Inc = 0x0; $charAt64Inc + 64 < $baseDecodedLength; $charAt64Inc++) {
+            $_0x47a1c9 = ($_0x47a1c9 + $_0x517f5a) % 256;
+            $_0x17bc47 = ($_0x206008 + $tabOfKey[($_0x17bc47 + $tabOfKey[$_0x47a1c9]) % 256]) % 256;
+            $_0x206008 = ($_0x206008 + $_0x47a1c9 + $tabOfKey[$_0x47a1c9]) % 256;
+            $_0x734e1c = $tabOfKey[$_0x47a1c9];
+            $tabOfKey[$_0x47a1c9] = $tabOfKey[$_0x17bc47];
+            $tabOfKey[$_0x17bc47] = $_0x734e1c;
+            $_0xcd3dde = $tabOfKey[($_0x17bc47 + $tabOfKey[($_0x47a1c9 + $tabOfKey[($_0xcd3dde + $_0x206008) % 256]) % 256]) % 256];
+            $jsonToParse .= chr(ord($baseDecoded[$charAt64Inc + 64]) ^ $_0xcd3dde);
+        }
+        return \GuzzleHttp\json_decode($jsonToParse, true);
     }
 }
